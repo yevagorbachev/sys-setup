8aec2189a98c2179ed5dcf2470693395d0688c60
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2023-10-30 13:48:52 -0400

    Snippet updates

--- wsl/.config/nvim/LuaSnip/matlab.lua
+++ wsl/.config/nvim/LuaSnip/matlab.lua
@@ -10,7 +10,16 @@ local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
+local cap = function(idx)
+	return ls.function_node(function(_, snip)
+		return snip.captures[idx]
+	end)
+end
+
 return {
 	s({trig = "uass"}, fmta("<> = <>; % [<>]", {i(1), i(2), i(3)})),
-	s({trig = "ulab"}, fmta("$<> \\,\\mathrm{[<>]}$", {i(1), i(2)}))
+	s({trig = "([%a][_%w]* =)", regTrig = true}, fmta("<><>; % [<>]",
+		{cap(1), i(1), i(2)})),
+	s({trig = "ulab"}, fmta("\"$<>$ [<>]\"", {i(1), i(2)})),
+	s({trig = "fig"}, fmta("figure(\"name\", \"<>\");", {i(1)})),
 }
