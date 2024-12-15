833f1e85c93b9cb1c4b76d877e9a9865a69a02cf
Yevgeniy Gorbachev <yevgeniy.gorbachev@gmail.com>
2024-12-15 12:29:39 -0500

    Added winget script
    Neovim and prerequisites for MATLAB language server

---
+++ windows/windows-setup.ps1
@@ -0,0 +1,12 @@
+winget install --id Git.Git
+winget install --id cURL.cURL
+winget install --id Neovim.Neovim.Nightly
+winget install --id equalsraf.neovim-qt
+winget install --id zig.zig
+winget install --id OpenJS.NodeJS.LTS --version 16.17.1
+
+# $sysSetupRoot = "https://raw.githubusercontent.com/yevagorbachev/sys-setup/refs/heads/main/"
+# $vimrcSource = $sysSetupRoot + "/wsl/vimrc"
+#
+# curl -o $(Join-Path $env:USERPROFILE .vimrc) $sysSetupRoot + "/wsl/.vimrc"
+
