fc4bad8f1bc21102cc513637ca2e48ea251b8417
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2025-09-01 13:23:56 -0400

    re-added oh-my-zsh download to init

--- init-wsl.sh
+++ init-wsl.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # installs
-sudo apt clean
-# sudo add-apt-repository ppa:neovim-ppa/stable
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update -y
-sudo apt upgrade -y
+apt clean
+add-apt-repository ppa:neovim-ppa/unstable -y
+apt update -y
+apt upgrade -y
 
 # packages
-packages="stow tree neovim python3 python3-pip zig texlive-base texlive-font-utils texlive-science texlive-extra-utils texlive-latex-extra latexmk sl gdb zsh"
-sudo apt install -y $packages
+packages="stow tree vim neovim python3 python3-pip latexmk sl gdb zsh texlive-base texlive-font-utils texlive-science texlive-extra-utils texlive-latex-extra"
+apt install -y $packages
 
 # shell
-# yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Prompt to change shell to zsh"
 chsh -s $(which zsh)
 mkdir ~/.projects
 
@@ -19,7 +19,7 @@ read -p "Enter seed for SSH key: " SEED
 ssh-keygen -t rsa -b 4096 -C SEED
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-echo "SSH key:"
+echo "SSH public key:"
 cat ~/.ssh/id_rsa.pub
 
 echo "Execute 'stow wsl' from ~/sys-setup to link dotfiles"
